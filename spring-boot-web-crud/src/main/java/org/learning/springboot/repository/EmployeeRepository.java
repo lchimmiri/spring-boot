1258df4c6ec15dad14cffbbe4a0ed1457baa1ab3
Naidu Chimmiri <chimmiri.naidu@gmail.com>
2020-09-26 17:59:46 -0500

    spring-boot web crud application

---
+++ spring-boot-web-crud/src/main/java/org/learning/springboot/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package org.learning.springboot.repository;
+
+import org.learning.springboot.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+	
+}
---
+++ spring-boot-web-crud/src/main/java/org/learning/springboot/service/EmployeeService.java
@@ -0,0 +1,19 @@
+package org.learning.springboot.service;
+
+import java.util.List;
+
+import org.learning.springboot.model.Employee;
+import org.springframework.data.domain.Page;
+
+public interface EmployeeService {
+
+	List<Employee> getAllEmployees();
+
+	void saveEmployee(Employee employee);
+
+	Employee getEmployeeById(long id);
+
+	void deleteEmployeeById(long id);
+
+	Page<Employee> findPaginated(int pageNo, int pageSize, String sortField, String sortDirection);
+}
