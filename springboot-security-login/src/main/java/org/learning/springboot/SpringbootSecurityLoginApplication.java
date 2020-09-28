01e03c0c7912957593b358b4022814c9e6d14fee
Naidu Chimmiri <chimmiri.naidu@gmail.com>
2020-09-28 17:55:08 -0500

    Spring boot security login app

---
+++ springboot-security-login/src/main/java/org/learning/springboot/SpringbootSecurityLoginApplication.java
@@ -0,0 +1,13 @@
+package org.learning.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSecurityLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSecurityLoginApplication.class, args);
+	}
+
+}
