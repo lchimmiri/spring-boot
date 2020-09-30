0de797054d681d7ca4b01cc348182618289cb2de
Naidu Chimmiri <chimmiri.naidu@gmail.com>
2020-09-30 09:12:25 -0500

    feat(emp-rest) add create employee feature

--- springboot-backend/src/main/java/org/learning/springboot/controller/EmployeeController.java
+++ springboot-backend/src/main/java/org/learning/springboot/controller/EmployeeController.java
@@ -5,10 +5,14 @@ import java.util.List;
 import org.learning.springboot.model.Employee;
 import org.learning.springboot.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1")
 public class EmployeeController {
@@ -20,5 +24,11 @@ public class EmployeeController {
 	@GetMapping("/employees")
 	public List<Employee> getAllEmployees(){
 		return employeeRepository.findAll();
+	}
+	
+	//Add Employee
+	@PostMapping("/employees")
+	public Employee createEmployee(@RequestBody Employee employee) {
+		return employeeRepository.save(employee);
 	}
 }
