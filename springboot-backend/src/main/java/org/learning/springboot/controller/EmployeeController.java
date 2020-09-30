e33ddf1aa307fec4cb65776f9dfcc9d25e8dc4d8
Naidu Chimmiri <chimmiri.naidu@gmail.com>
2020-09-30 10:13:17 -0500

    added getempbyid feature

--- springboot-backend/src/main/java/org/learning/springboot/controller/EmployeeController.java
+++ springboot-backend/src/main/java/org/learning/springboot/controller/EmployeeController.java
@@ -2,11 +2,14 @@ package org.learning.springboot.controller;
 
 import java.util.List;
 
+import org.learning.springboot.exception.ResourceNotFoundException;
 import org.learning.springboot.model.Employee;
 import org.learning.springboot.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,16 +22,24 @@ public class EmployeeController {
 
 	@Autowired
 	private EmployeeRepository employeeRepository;
-	
-	//get all Employees
+
+	// get all Employees
 	@GetMapping("/employees")
-	public List<Employee> getAllEmployees(){
+	public List<Employee> getAllEmployees() {
 		return employeeRepository.findAll();
 	}
-	
-	//Add Employee
+
+	// Add Employee
 	@PostMapping("/employees")
 	public Employee createEmployee(@RequestBody Employee employee) {
 		return employeeRepository.save(employee);
+	}
+
+	// get employee by id
+	@GetMapping("/employees/{id}")
+	public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+		Employee employee= employeeRepository.findById(id)
+				.orElseThrow(()-> new ResourceNotFoundException("Employee Not Found with Id : "+id));
+		return ResponseEntity.ok(employee);
 	}
 }
